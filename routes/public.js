e633a16f9ef0fff495ca60c8aded2ee5f836985e
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-07-18 00:44:29 +0200

    Calendar done

--- routes/public.js
+++ routes/public.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const view = req.query.view || "index";
-  const extraData = {twitch: valueTwitch, kick: valueKick};
-  res.render("main", {view: view, data: extraData});
+  res.render("main", {view: view});
 });
 
 export default router;
