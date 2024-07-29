03eecd677a900f8ea9e5a8c4cfa9a22b292224a2
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-07-29 02:03:39 +0200

    fix deattach

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 docker build -t alexdevuwu-web .
-docker run -p 3003:3000 -v /alexdevuwu-web/data:/data -v /alexdevuwu-web/env:/env alexdevuwu-web
+docker run -d -p 3003:3000 -v /alexdevuwu-web/data:/data -v /alexdevuwu-web/env:/env alexdevuwu-web
