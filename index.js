a32e689a1c218e5d8ec8aa1a99b4b3ba28998a79
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-07-29 02:20:14 +0200

    delete print

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-import fs from "fs";
 
 // Configuración del servidor Express
 const app = express();
@@ -15,14 +14,6 @@ const envFilePath = path.join(__dirname, "env", ".env");
 dotenv.config({ path: envFilePath });
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-fs.readFile(envFilePath, "utf8", (err, data) => {
-  if (err) {
-    console.error(`Error al leer el archivo .env: ${err}`);
-  } else {
-    console.log(`Contenido del archivo .env:\n${data}`);
-  }
-});
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, "public")));
