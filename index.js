92202133bc7f1a7efec3da373faaf13f734b5258
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-07-29 02:09:42 +0200

    jo, fix env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 // Configuración del servidor Express
 const app = express();
@@ -12,15 +13,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const envFilePath = path.join(__dirname, "env", ".env");
 dotenv.config({ path: envFilePath });
-console.log(process.env);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+fs.readFile(envFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Error al leer el archivo .env: ${err}`);
+  } else {
+    console.log(`Contenido del archivo .env:\n${data}`);
+  }
+});
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Manejador de errores
 app.use((err, req, res, next) => {
