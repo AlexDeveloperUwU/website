71d777dda56a50af70c0016662dfd56bf507b2c9
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-07-29 10:02:25 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,5 @@
+git pull
+docker stop alexdevuwu-web && docker rm alexdevuwu-web
+docker images -q alexdevuwu-web | xargs docker rmi
 docker build -t alexdevuwu-web .
-docker run -d -p 3003:3000 -v /alexdevuwu-web/data:/usr/src/app/data -v /alexdevuwu-web/env:/usr/src/app/env alexdevuwu-web
+docker run -d --name alexdevuwu-web -p 3003:3000 -v /alexdevuwu-web/data:/usr/src/app/data -v /alexdevuwu-web/env:/usr/src/app/env alexdevuwu-web
