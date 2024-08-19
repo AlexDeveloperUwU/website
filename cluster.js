28d75e48d6d2cb4a0f014d6906ab98ac5fd055ae
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-08-19 17:35:01 +0200

    try to cluster

---
+++ cluster.js
@@ -0,0 +1,19 @@
+import cluster from "cluster";
+import os from "os";
+
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new one.`);
+    cluster.fork();
+  });
+} else {
+  // Importar y ejecutar el servidor en cada worker
+  await import("./index.js");
+}
