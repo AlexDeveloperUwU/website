5de08772325176987c0289cdcb649d4e4720afe1
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-08-22 20:36:14 +0200

    reorganización de carpetas de las views

--- routes/public.js
+++ routes/public.js
@@ -1,60 +1,31 @@
-import express from "express";
-import { promises as fs } from "fs"; // Usando `promises` para trabajar con promesas en lugar de callbacks
+import { promises as fs } from "fs";
 import path from "path";
+import express from "express";
 import { fileURLToPath } from "url";
-import { getLink } from "../utils/db.js";
 
 const router = express.Router();
+
+// Convierte import.meta.url a una ruta de sistema de archivos
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const titlesPath = path.resolve(__dirname, "../public/json/titles.json");
 
-// Ruta para manejar los errores
-router.get("/error", async (req, res) => {
-  const view = req.query.code || "404";
+// Usa __dirname para construir rutas
+const contentRoutesPath = path.join(__dirname, "..", "server-utils", "contentRoutes.json");
+const adminRoutesPath = path.join(__dirname, "..", "server-utils", "adminRoutes.json");
+const errorRoutesPath = path.join(__dirname, "..", "server-utils", "errorRoutes.json");
 
-  try {
-    const data = await fs.readFile(titlesPath, "utf8");
-    const titles = JSON.parse(data);
-
-    if (titles[view]) {
-      const pageTitle = titles[view];
-      res.render("errors", { view, pageTitle });
-    } else {
-      res.redirect("/error?code=404");
-    }
-  } catch (err) {
-    const pageTitle = "Web Oficial";
-    res.render("errors", { view: "index", pageTitle });
-  }
-});
-
-// Ruta para manejar redirecciones
-router.get("/l/:id?", async (req, res) => {
-  const id = req.params.id || req.query.id;
-
-  if (!id) {
-    return res.redirect("/error?code=400");
-  }
+router.get(["/admin/:viewName", "/admin"], async (req, res) => {
+  const viewName = req.params.viewName || req.query.view || "index";
 
   try {
-    let linkData = getLink(id);
+    const adminRoutesData = await fs.readFile(adminRoutesPath, "utf8");
+    const adminRoutes = JSON.parse(adminRoutesData);
 
-    if (linkData) {
-      const destinationUrl = new URL(linkData);
-      destinationUrl.searchParams.set("referrer", "alexdevuwu.com");
+    const routeData = adminRoutes.routes.find((route) => route.view === viewName);
 
-      if (
-        destinationUrl.hostname === "www.youtube.com" ||
-        destinationUrl.hostname === "youtube.com"
-      ) {
-        res.setHeader("X-Robots-Tag", "noindex, nofollow");
-        res.setHeader("Referrer-Policy", "no-referrer");
-      } else {
-        res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
-      }
-
-      res.redirect(destinationUrl.toString());
+    if (routeData) {
+      const { title, folder } = routeData;
+      res.render("main", { route: folder, view: viewName, title, type: "admin" });
     } else {
       res.redirect("/error?code=404");
     }
@@ -63,34 +34,45 @@ router.get("/l/:id?", async (req, res) => {
   }
 });
 
-// Ruta para el archivo robots.txt
-router.get("/robots.txt", (req, res) => {
-  res.status(404).end();
+router.get(["/error/:viewName", "/error"], async (req, res) => {
+  const code = req.params.code || req.query.code;
+
+  try {
+    const errorRoutesData = await fs.readFile(errorRoutesPath, "utf8");
+    const errorRoutes = JSON.parse(errorRoutesData);
+
+    const routeData = errorRoutes.routes.find((route) => route.view === code);
+
+    if (routeData) {
+      const { title, folder } = routeData;
+      res.render("main", { route: folder, code, title, view: code, type: "error" });
+    } else {
+      res.redirect("/error?code=500");
+    }
+  } catch (err) {
+    console.error("Error reading or parsing errorRoutes.json:", err);
+    res.redirect("/error?code=500");
+  }
 });
 
-// Ruta universal para redirigir cuando no se proporciona un `view` válido
+//! ESTA RUTA SIEMPRE DEBE IR AL FINAL
 router.get(["/:viewName", "/"], async (req, res) => {
   const viewName = req.params.viewName || req.query.view || "index";
 
-  const extraQueries = { ...req.query };
-  delete extraQueries.view;
-
   try {
-    const data = await fs.readFile(titlesPath, "utf8");
-    const titles = JSON.parse(data);
+    const contentRoutesData = await fs.readFile(contentRoutesPath, "utf8");
+    const contentRoutes = JSON.parse(contentRoutesData);
 
-    if (titles[viewName]) {
-      if (req.params.viewName) {
-        res.redirect(`/?view=${viewName}`);
-      } else {
-        const pageTitle = titles[viewName];
-        res.render("main", { view: viewName, pageTitle, data: extraQueries });
-      }
+    const routeData = contentRoutes.routes.find((route) => route.view === viewName);
+
+    if (routeData) {
+      const { title, folder } = routeData;
+      res.render("main", { route: folder, view: viewName, title, type: "content" });
     } else {
       res.redirect("/error?code=404");
     }
   } catch (err) {
-    res.redirect("/error?code=500");
+    res.redirect("/error?code=404");
   }
 });
 
