80bf57b8e562699e87e545228efcbe77f07e1e24
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-08-25 17:03:27 +0200

    tech stack w/ animation + discord status in copy card

--- public/js/content/discord.js
+++ public/js/content/discord.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   function fetchDiscordStatus() {
-    fetch("https://api.lanyard.rest/v1/users/419176939497193472")
+    return fetch("https://api.lanyard.rest/v1/users/419176939497193472")
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          const status = data.data.discord_status;
-          const borderColor = getBorderColor(status);
-
-          document.getElementById("profileImage").style.borderColor = borderColor;
+          return data.data.discord_status;
+        } else {
+          throw new Error("Failed to fetch Discord status");
         }
       })
-      .catch((error) => console.error("Error fetching Discord status:", error));
+      .catch((error) => {
+        console.error("Error fetching Discord status:", error);
+        throw error;
+      });
   }
 
   function getBorderColor(status) {
@@ -28,6 +30,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  fetchDiscordStatus();
-  setInterval(fetchDiscordStatus, 60000);
+  function updateProfileBorderColor() {
+    fetchDiscordStatus()
+      .then((status) => {
+        const borderColor = getBorderColor(status);
+        document.getElementById("profileImage").style.borderColor = borderColor;
+      })
+      .catch((error) => console.error("Error updating profile border color:", error));
+  }
+
+  function updateDiscordStatusCard() {
+    fetchDiscordStatus()
+      .then((status) => {
+        let statusSpanish = "";
+        switch (status) {
+          case "online":
+            statusSpanish = "conectado";
+            break;
+          case "idle":
+            statusSpanish = "ausente";
+            break;
+          case "dnd":
+            statusSpanish = "ocupado";
+            break;
+          case "offline":
+            statusSpanish = "desconectado";
+            break;
+          default:
+            statusSpanish = "Desconocido";
+        }
+
+        const statusElement = document.getElementById("myDiscordStatus");
+        statusElement.textContent = statusSpanish;
+      })
+      .catch((error) => console.error("Error updating Discord status card:", error));
+  }
+
+  updateProfileBorderColor();
+  updateDiscordStatusCard();
+  setInterval(updateProfileBorderColor, 60000);
+  setInterval(updateDiscordStatusCard, 60000);
 });
--- public/js/content/techStack.js
+++ public/js/content/techStack.js
@@ -1,134 +1,40 @@
 $(document).ready(function () {
   function loadTechStack(type) {
-    let content = "";
+    const stackInfo = $("#stackInfo");
+    stackInfo.addClass("fade-out");
 
-    if (type === "noUI") {
-      content = `
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-cogs mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Motor</h4>
-              <p class="text-gray-400 font-agrandir">NodeJS</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-desktop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Entorno de Desarrollo</h4>
-              <p class="text-gray-400 font-agrandir">Visual Studio Code</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-database mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Bases de Datos</h4>
-              <p class="text-gray-400 font-agrandir">SQLite (Enmap)</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-box mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Herramientas de Containerización</h4>
-              <p class="text-gray-400 font-agrandir">Docker</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-laptop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Sistemas Operativos</h4>
-              <p class="text-gray-400 font-agrandir">Windows / Linux</p>
-            </div>
-          </div>`;
-    } else if (type === "webUI") {
-      content = `
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-cogs mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Motor</h4>
-              <p class="text-gray-400 font-agrandir">NodeJS</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-desktop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Entorno de Desarrollo</h4>
-              <p class="text-gray-400 font-agrandir">Visual Studio Code</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-code mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Frontend</h4>
-              <p class="text-gray-400 font-agrandir">HTML, CSS, JavaScript</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-cogs mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Frameworks</h4>
-              <p class="text-gray-400 font-agrandir">TailwindCSS, Express, EJS</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-database mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Bases de Datos</h4>
-              <p class="text-gray-400 font-agrandir">SQLite (Enmap)</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-box mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Herramientas de Containerización</h4>
-              <p class="text-gray-400 font-agrandir">Docker</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-laptop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Sistemas Operativos</h4>
-              <p class="text-gray-400 font-agrandir">Windows / Linux</p>
-            </div>
-          </div>`;
-    } else if (type === "java") {
-      content = `
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-cogs mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Motor</h4>
-              <p class="text-gray-400 font-agrandir">Java (Versión 21)</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-desktop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Entorno de Desarrollo</h4>
-              <p class="text-gray-400 font-agrandir">JetBrains IntelliJ</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-database mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Bases de Datos</h4>
-              <p class="text-gray-400 font-agrandir">MySQL (si necesita)</p>
-            </div>
-          </div>
-          <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
-            <i class="fas fa-laptop mr-4 text-gray-300 text-xl"></i>
-            <div>
-              <h4 class="font-bold text-gray-100">Sistemas Operativos</h4>
-              <p class="text-gray-400 font-agrandir">Windows / Linux</p>
-            </div>
-          </div>`;
-    }
+    setTimeout(() => {
+      $.getJSON("/json/techStacks.json", function (data) {
+        let content = "";
 
-    $("#stackInfo").html(content);
+        if (data[type]) {
+          data[type].forEach((item) => {
+            content += `
+              <div class="p-6 bg-gray-800 rounded-lg shadow-lg flex items-center">
+                <i class="${item.icon} mr-4 text-gray-300 text-xl"></i>
+                <div>
+                  <h4 class="font-bold text-gray-100">${item.title}</h4>
+                  <p class="text-gray-400 font-agrandir">${item.description}</p>
+                </div>
+              </div>`;
+          });
+        }
+
+        stackInfo.html(content);
+        stackInfo.removeClass("fade-out");
+        stackInfo.addClass("fade-in");
+
+        setTimeout(() => {
+          stackInfo.removeClass("fade-in");
+        }, 600);
+      });
+    }, 600);
   }
+
+  loadTechStack($("#projectType").val());
 
   $("#projectType").change(function () {
     const selectedType = $(this).val();
     loadTechStack(selectedType);
   });
-
-  loadTechStack($("#projectType").val());
 });
