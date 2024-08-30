cccd8606ba5d2a8f83208a3e61c5df948cb0e2db
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-08-30 13:42:54 +0200

    new api endpoint for logs

--- public/js/common/mainIndex.js
+++ public/js/common/mainIndex.js
@@ -1,6 +1,5 @@
 function showMainContent() {
   const mainContent = document.querySelector("main");
-  console.log("showMainContent: Mostrando el contenido principal.");
   mainContent.classList.remove("hidden");
   mainContent.classList.add("fade-in");
 }
@@ -13,12 +12,10 @@ window.addEventListener("load", function () {
 
 function fadeOutMainContent(callback) {
   const mainContent = document.querySelector("main");
-  console.log("fadeOutMainContent: Iniciando desvanecimiento del contenido principal.");
   mainContent.classList.remove("fade-in");
   mainContent.classList.add("fade-out");
 
   setTimeout(function () {
-    console.log("setTimeout: El contenido principal se ocultará después de 605 ms.");
     mainContent.classList.add("hidden");
     mainContent.classList.remove("fade-out");
     callback();
@@ -26,7 +23,6 @@ function fadeOutMainContent(callback) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOMContentLoaded: El contenido del documento está completamente cargado.");
   document.addEventListener("click", function (event) {
     const link = event.target.closest('a[href*="?view="]');
 
@@ -43,8 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
 window.addEventListener("pageshow", function (event) {
   if (event.persisted) {
     console.log("pageshow event: La página se ha cargado desde la caché.");
+    showMainContent();
   } else {
     console.log("pageshow event: La página se ha cargado normalmente.");
   }
-  showMainContent();
 });
--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,14 @@
 import express from "express";
-import { apiAlert, formSend } from "../serverUtils/js/webhook.js";
-import {
-  addEvent,
-  removeEvent,
-  getAllEvents,
-  addLink,
-  editLink,
-  removeLink,
-  getAllLinks,
-  getLink,
-} from "../serverUtils/js/db.js";
+import * as webhook from "../serverUtils/js/webhook.js";
+import * as db from "../serverUtils/js/db.js";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
+import fs from "fs";
 import path from "path";
 import axios from "axios";
 
 const envPath = path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "..", "env", ".env");
+const logsDirectory = path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "logs");
 dotenv.config({ path: envPath });
 
 const router = express.Router();
@@ -32,19 +25,19 @@ const authenticateDiscord = (req, res, next) => {
 
 // Ruta para el formulario de contacto
 router.post("/contactForm", (req, res) => {
-  formSend(req.body);
+  webhook.formSend(req.body);
   res.status(200).json({ message: "Datos del formulario recibidos correctamente." });
 });
 
 // Ruta para obtener todos los eventos (solo información básica)
 router.get("/allEvents", (req, res) => {
-  const events = getAllEvents("simple");
+  const events = db.getAllEvents("simple");
   res.status(200).json(events);
 });
 
 // Ruta para obtener todos los links
 router.get("/allLinks", (req, res) => {
-  const links = getAllLinks();
+  const links = db.getAllLinks();
   res.status(200).json(links);
 });
 
@@ -53,18 +46,18 @@ router.get("/allLinks", (req, res) => {
 // Ruta para añadir un evento
 router.post("/addEvent", authenticateDiscord, (req, res) => {
   const event = req.body;
-  const id = addEvent(event);
-  apiAlert("eventAdded", { id, date: event.date, time: event.time });
+  const id = db.addEvent(event);
+  webhook.apiAlert("eventAdded", { id, date: event.date, time: event.time });
   res.status(200).json({ message: `Event with ID ${id} added successfully` });
 });
 
 // Ruta para eliminar un evento
 router.post("/removeEvent", authenticateDiscord, (req, res) => {
   const event = req.body;
-  const result = removeEvent(event);
+  const result = db.removeEvent(event);
   if (result) {
     res.status(200).json({ message: "Event removed successfully" });
-    apiAlert("eventRemoved", { id: event.id, date: event.date, time: event.time });
+    webhook.apiAlert("eventRemoved", { id: event.id, date: event.date, time: event.time });
   } else {
     res.status(404).json({ error: "Event not found" });
   }
@@ -72,48 +65,48 @@ router.post("/removeEvent", authenticateDiscord, (req, res) => {
 
 // Ruta para gestionar todos los eventos (información completa)
 router.get("/manageAllEvents", authenticateDiscord, (req, res) => {
-  const events = getAllEvents("full");
+  const events = db.getAllEvents("full");
   res.status(200).json(events);
 });
 
 // Ruta para añadir un link
 router.post("/addLink", authenticateDiscord, (req, res) => {
   const { id, url } = req.body;
-  const result = addLink(id, url);
+  const result = db.addLink(id, url);
 
   if (result === false) {
     res.status(400).json({ message: `Link with ID ${id} already exists` });
   } else if (typeof result === "string") {
     res.status(200).json({ message: `Link added successfully with generated ID ${result}` });
-    apiAlert("linkAdded", { id: result, url });
+    webhook.apiAlert("linkAdded", { id: result, url });
   } else {
     res.status(200).json({ message: `Link with ID ${id} added successfully` });
-    apiAlert("linkAdded", { id, url });
+    webhook.apiAlert("linkAdded", { id, url });
   }
 });
 
 // Ruta para editar un link existente
 router.post("/editLink", authenticateDiscord, (req, res) => {
   const { id, url } = req.body;
-  const result = editLink(id, url);
+  const result = db.editLink(id, url);
   if (result === false) {
     res.status(404).json({ message: `Link with ID ${id} not found` });
   } else {
     res.status(200).json({ message: `Link with ID ${id} edited successfully` });
-    apiAlert("linkEdited", { id, url });
+    webhook.apiAlert("linkEdited", { id, url });
   }
 });
 
 // Ruta para eliminar un link
 router.post("/removeLink", authenticateDiscord, (req, res) => {
   const { id } = req.body;
-  const url = getLink(id);
-  const result = removeLink(id);
+  const url = db.getLink(id);
+  const result = db.removeLink(id);
   if (result === false) {
     res.status(404).json({ message: `Link with ID ${id} not found` });
   } else {
     res.status(200).json({ message: `Link with ID ${id} removed successfully` });
-    apiAlert("linkRemoved", { id, url });
+    webhook.apiAlert("linkRemoved", { id, url });
   }
 });
 
@@ -151,6 +144,34 @@ router.get("/serverStats", async (req, res) => {
     } else {
       res.status(500).send("Error fetching server stats");
     }
+  }
+});
+
+router.get("/latestLog", authenticateDiscord, async (req, res) => {
+  try {
+    const files = fs.readdirSync(logsDirectory);
+    const logFiles = files.filter((file) => file.endsWith(".log"));
+
+    if (logFiles.length === 0) {
+      return res.status(404).json({ message: "No log files found" });
+    }
+
+    logFiles.sort((a, b) => {
+      const aPath = path.join(logsDirectory, a);
+      const bPath = path.join(logsDirectory, b);
+      return fs.statSync(bPath).mtime - fs.statSync(aPath).mtime;
+    });
+
+    const latestLogFile = logFiles[0];
+    const latestLogFilePath = path.join(logsDirectory, latestLogFile);
+
+    const logContent = fs.readFileSync(latestLogFilePath, "utf-8");
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(logContent);
+  } catch (error) {
+    console.error("Error fetching latest log file:", error);
+    res.status(500).json({ message: "Error fetching latest log file" });
   }
 });
 
