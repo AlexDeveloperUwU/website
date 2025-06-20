8e142a73179dacfcb65ea877e4284afab1d75312
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-06-20 13:39:54 +0200

    chore: :fire: Remove ALL website, rewrite with vue + express backend

--- public/js/common/adjustBodyPadding.js
+++
@@ -1,8 +0,0 @@
-function adjustBodyPadding() {
-  const header = document.getElementById("main-header");
-  const headerHeight = header.offsetHeight;
-  document.body.style.paddingTop = headerHeight - 50 + "px";
-}
-
-window.addEventListener("load", adjustBodyPadding);
-window.addEventListener("resize", adjustBodyPadding);
--- public/js/minified/common/adjustBodyPadding.js
+++
@@ -1,1 +0,0 @@
-function adjustBodyPadding(){const d=document.getElementById("main-header").offsetHeight;document.body.style.paddingTop=d-50+"px"}window.addEventListener("load",adjustBodyPadding),window.addEventListener("resize",adjustBodyPadding);
